f1f5352ae5efeee4f44b30f8406f359dc36cd0c8
Oliver-Fish <github@oliver.fish>
2018-07-30 07:22:25 +0200

    Resolved GoLint errors and Improved comments

--- newsapi.go
+++ newsapi.go
@@ -15,20 +15,22 @@ const (
 	apiEverythingPath = "/everything?"
 )
 
+type option func(*Client)
+
 type errorResponse struct {
 	Status  string `json:"status"`
 	Code    string `json:"code"`
 	Message string `json:"message"`
 }
 
+//Client contains the data required to make requests to NewsAPI
 type Client struct {
 	APIUrl     string
 	APIKey     string
 	HTTPClient *http.Client
 }
 
-type Option func(*Client)
-
+//Article contains data on an article returned from NewsAPI
 type Article struct {
 	Source      Source `json:"source"`
 	Author      string `json:"author"`
@@ -38,24 +40,28 @@ type Article struct {
 	URLToImage  string `json:"urlToImage"`
 	PublishedAt string `json:"publishedAt"`
 }
+
+//ArticleResults contains a slice of Articles along with it's length and the NewsAPI Status
 type ArticleResults struct {
 	Status       string    `json:"status"`
 	TotalResults int64     `json:"totalResults"`
 	Articles     []Article `json:"articles"`
 }
 
+//Source contains data on a source returned from NewsAPI
 type Source struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
 }
 
+//SourceResults contains a slice of Articles the NewsAPI Status
 type SourceResults struct {
 	Status string   `json:"status"`
 	Source []Source `json:"sources"`
 }
 
 //New creates our Client struct that we use to make requests
-func New(apiKey string, options ...Option) *Client {
+func New(apiKey string, options ...option) *Client {
 	c := Client{
 		APIUrl: apiPath,
 		APIKey: apiKey,
@@ -70,6 +76,7 @@ func New(apiKey string, options ...Option) *Client {
 }
 
 //makeRequest handles all our requests to newsapi
+//Takes a request URL and returns the replied body and an error from either the request or NewsAPI
 func (c *Client) makeRequest(endpoint string) ([]byte, error) {
 	req, err := http.NewRequest("GET", endpoint, nil)
 	if err != nil {
@@ -98,6 +105,15 @@ func (c *Client) makeRequest(endpoint string) ([]byte, error) {
 	return b, nil
 }
 
+//GetTopHeadlines is used to interact with the TopHeadlines API endpoint
+//https://newsapi.org/docs/endpoints/top-headlines
+//This takes the following paramaters with the accepted types
+// country - string
+// category - string
+// sources - []string
+// q - string
+// pageSize - int
+// page - int
 func (c *Client) GetTopHeadlines(p parameters) (ArticleResults, error) {
 	var o ArticleResults
 	ap := allowedParameters{ //List of allowed parameters and their allowed types
@@ -123,6 +139,18 @@ func (c *Client) GetTopHeadlines(p parameters) (ArticleResults, error) {
 	return o, nil
 }
 
+//GetEverything is used to interact with the GetEverything API endpoint
+//https://newsapi.org/docs/endpoints/everything
+//This takes the following paramaters with the accepted types
+// q - string
+// sources - []string
+// domains - []string
+// from - string
+// to - string
+// lanague - string
+// sortBy - string
+// pageSize - int
+// page - int
 func (c *Client) GetEverything(p parameters) (ArticleResults, error) {
 	var o ArticleResults
 	ap := allowedParameters{ //List of allowed parameters and their allowed types
@@ -150,6 +178,12 @@ func (c *Client) GetEverything(p parameters) (ArticleResults, error) {
 	return o, nil
 }
 
+//GetSources is used to interact with the GetSources API endpoint
+//https://newsapi.org/docs/endpoints/sources
+//This takes the following paramaters with the accepted types
+// country - string
+// category - string
+// lanague - string
 func (c *Client) GetSources(p parameters) (SourceResults, error) {
 	var o SourceResults
 	ap := allowedParameters{ //List of allowed parameters and their allowed types
